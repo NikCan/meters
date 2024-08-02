24b38c0285aa582fa0155ea57aef0a81e23b0e54
NikCan <nikitagaponov@gmail.com>
2024-08-02 13:04:59 +0300

    https

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,6 +1,6 @@
 export const pagesPerPage = 20;
 
-const baseUrl = 'http://showroom.eis24.me/api/v4/test';
+const baseUrl = 'https://showroom.eis24.me/api/v4/test';
 
 export const apiConfig = {
   METERS: `${baseUrl}/meters`,
